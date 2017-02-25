fbd07c1d4fcc25e19bad8d079590ad10e6430cad
leor77 <leorbenari@yahoo.com>
2017-02-24 16:10:21 -0800

    Added new VC, "SpotLocationVC", which is triggered when someone clicks on one of the cells to purchase a spot.

--- ParkMe App for Seller/RequestVC.swift
+++ ParkMe App for Seller/RequestVC.swift
@@ -6,27 +6,20 @@ import FirebaseDatabase
 import MapKit
 import CoreLocation
 
-struct user {
-    var userName: String
-    var latitude: Double
-    var longitude: Double
-}
-
 class RequestVC: UITableViewController, CLLocationManagerDelegate {
     
     var locationManager = CLLocationManager()
-    
     var sellerUserNames = [String]()
-    var sellerLongitudes = [Double]()
-    var sellerLatitudes = [Double]()
-    
     var requestLocations = [CLLocationCoordinate2D]()
     
-    var userList = [user]()
-
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "backToMain" {
             self.navigationController?.navigationBar.isHidden = true
+        }
+        else if segue.identifier == "showSpotLocation" {
+            
+            let rowNumber = self.tableView.indexPathForSelectedRow?.row
+            print(rowNumber)
         }
     }
     
@@ -62,19 +55,7 @@ class RequestVC: UITableViewController, CLLocationManagerDelegate {
 
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-
-        let location = locationManager.location?.coordinate
         }
-
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-
-    // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
@@ -111,51 +92,4 @@ class RequestVC: UITableViewController, CLLocationManagerDelegate {
      
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ ParkMe App for Seller/SpotLocationVC.swift
@@ -0,0 +1,24 @@
+
+import UIKit
+import MapKit
+import FirebaseDatabase
+
+class SpotLocationVC: UIViewController, MKMapViewDelegate {
+    
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    @IBOutlet var purchaseSpotButton: UIButton!
+    
+    @IBAction func purchaseSpot(_ sender: Any) {
+    }
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
