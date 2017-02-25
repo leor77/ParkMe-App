91ed9564674a89da02b66ba16acbe565c05d4598
leor77 <leorbenari@yahoo.com>
2017-02-24 17:26:11 -0800

    Added functionality for buying parking spot. As of right now, it takes the user's location and gives directions via apple maps. Still need to notify the user, and ask them to accept this user.

--- ParkMe App for Seller/RiderVC.swift
+++ ParkMe App for Seller/RiderVC.swift
@@ -91,14 +91,6 @@ class RiderVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, P
         }
     }
     
-    
-    @IBAction func requestSpot(_ sender: Any) {
-        
-        RequesterHandler.Instance.delegate = self;
-        RequesterHandler.Instance.listenToRequests();
-        
-    }
-    
     func acceptSpot(lat: Double, long: Double) {
 
         parkingSpotRequest(title: "Spot Request", message: "You have a spot available at this location: Lat: \(lat), Long: \(long)", requestAlive: true);
--- ParkMe App for Seller/SpotLocationVC.swift
+++ ParkMe App for Seller/SpotLocationVC.swift
@@ -13,6 +13,49 @@ class SpotLocationVC: UIViewController, MKMapViewDelegate {
     @IBOutlet var mapView: MKMapView!
     @IBOutlet var purchaseSpotButton: UIButton!
     @IBAction func purchaseSpot(_ sender: Any) {
+        
+        // let ref = FIRDatabase.database().reference()
+        
+        let buyerLocation = CLLocation(latitude: self.requestLocation.latitude, longitude: self.requestLocation.longitude)
+        
+        CLGeocoder().reverseGeocodeLocation(buyerLocation, completionHandler: { (placemarks, error) in
+            
+            if let placemarks = placemarks {
+                if placemarks.count > 0 {
+                    let mKPlacemark = MKPlacemark(placemark: placemarks[0])
+                    
+                    let mapItem = MKMapItem(placemark: mKPlacemark)
+                    mapItem.name = self.requestUsername
+                    
+                    let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+                    
+                    mapItem.openInMaps(launchOptions: launchOptions)
+                    
+                }
+            }
+            
+        })
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //ref.queryOrdered(byChild: "Sell_Request").queryEqual(toValue: requestUsername).
+            
+//            { (FIRDataSnapshot) in
+//            
+//            if let data = FIRDataSnapshot.value as? NSDictionary {
+//                if let name = data[Constants.NAME] as? String {
+
+        
     }
     
     override func viewDidLoad() {
